a43ab7c03667c83cae1b5b7c6f6d8a7d4635eaba
Hay Kranen <huskyr@gmail.com>
2019-06-01 00:01:29 +0200

    Adding -3d option

--- test/test-all.py
+++ test/test-all.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-import subprocess
 from tests import TESTS
+import argparse
+import subprocess
+import sys
 
 def cmd(cmd):
     subprocess.check_call(cmd, shell = True)
@@ -15,8 +17,22 @@ def run_test(test):
     cmd(f"cd ../ && ./facetool.py {test['command']} -vv")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--group")
+    parser.add_argument("-3d", "--three-dee", action = "store_true")
+    args = parser.parse_args()
+
     cleanup()
 
     for test in TESTS:
+        if args.group:
+            group = test["command"].split(" ")[0]
+
+            if group != args.group:
+                continue
+
+        if args.three_dee:
+            test["command"] += " --swap-method faceswap3d"
+
         print(f"\n*** {test['label']} ***\n")
         run_test(test)
